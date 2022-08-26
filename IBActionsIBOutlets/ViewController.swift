20c87ec8fbc96b7b1e6151ba9503075effaad9fd
Ivan Nieto Lozada <ivan.nieto@kavak.com>
2022-08-26 18:48:38 -0500

    Initial Commit

---
+++ IBActionsIBOutlets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBActionsIBOutlets
+//
+//  Created by IVAN NIETO on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
