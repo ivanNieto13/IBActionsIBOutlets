c1bb9a63400ae3c7fdead430628554bda6a6beb3
Ivan Nieto <ivannieto59@aragon.unam.mx>
2022-09-01 21:45:37 -0500

    feat: use ib actions/outlets

--- IBActionsIBOutlets/ViewController.swift
+++ IBActionsIBOutlets/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pokemonNumber: Int8 = 0
 
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // print("Se ejecutó la carga de la vista")
+        // messageLabel.text = "Nuevo mensaje"
     }
 
-
+    @IBAction func actionButtonn(_ sender: UIButton) {
+        messageLabel.text = "Pokémon #\(pokemonNumber)"
+        imageView.image = UIImage.init(named: String(pokemonNumber))
+        pokemonNumber = pokemonNumber + 1
+        
+    }
+    
+    @IBAction func changePokemon(_ sender: UIButton) {
+         messageLabel.text = "Pokémon #\(pokemonNumber)"
+        imageView.image = UIImage.init(named: String(pokemonNumber))
+        pokemonNumber = pokemonNumber + 1
+    }
 }
 
